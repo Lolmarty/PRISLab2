04a8f9de15e1558e9de8db4e0a7e25c975cbafef
Lolmarty <lysenko.andriy@gmail.com>
2016-04-09 16:24:15 +0300

    hoi, i'm temie

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+import copy
+
+from numpy.matrixlib.defmatrix import matrix
+
+
 class IntervalNumber:
     def __init__(self, low, high):
         self.low = low
@@ -27,19 +32,23 @@ class TrapezoidalNumber:
 
 
 class FuzzyPairwiseComparisonMatrix:
-    def __init__(self, n):
+    def __init__(self, n, matrix=None):
         self.n = n
-        self.matrix = [[None for _ in range(0, n)] for __ in range(0, n)]
+        self.matrix = copy.deepcopy(matrix) if matrix is not None else [[None if i != j else 1
+                                                                         for i in range(0, n)]
+                                                                        for j in range(0, n)]
 
     def __getitem__(self, i, j):
-        if self.matrix[i][j] is not None:
-            return self.matrix[i][j]
-        else:
-            return self.matrix[j][i].Inverse()
+        return self.matrix[i][j]
+
+    def AlphaLevel(self, alpha):
+        return FuzzyPairwiseComparisonMatrix(self.n,
+                                             [[self.matrix[i][j].AlphaLevel(alpha) if i != j else 1
+                                               for i in range(0, self.n)]
+                                              for j in range(0, self.n)])
 
 
-
-one = TrapezoidalNumber(1./3, 0.5, 1.5, 2)
+one = TrapezoidalNumber(1. / 3, 0.5, 1.5, 2)
 two = TrapezoidalNumber(1, 1.5, 2.5, 3)
 three = TrapezoidalNumber(2, 2.5, 3.5, 4)
 four = TrapezoidalNumber(3, 3.5, 4.5, 5)
@@ -48,3 +57,7 @@ six = TrapezoidalNumber(5, 5.5, 6.5, 7)
 seven = TrapezoidalNumber(6, 6.5, 7.5, 8)
 eight = TrapezoidalNumber(7, 7.5, 8.5, 9)
 nein = TrapezoidalNumber(8, 9, 9, 9)
+
+criteria_fpcm = FuzzyPairwiseComparisonMatrix(3, [[1, two.Inverse(), three.Inverse()],
+                                                  [two, 1, three.Inverse()],
+                                                  [three, three, 1]])
