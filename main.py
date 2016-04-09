b4e60ae8e2cfd921d6d0cf33eea5055153208edc
Lolmarty <lysenko.andriy@gmail.com>
2016-04-09 22:19:22 +0300

    wecvssrsvgtmwftwpg

--- main.py
+++ main.py
@@ -13,6 +13,9 @@ class IntervalNumber:
     def Inverse(self):
         return IntervalNumber(1. / self.high, 1. / self.low)
 
+    def DistanceToZero(self):
+        return (((self.low + self.high) / 2.) ** 2 + (((self.high - self.low) / 2.) ** 2) / 3) ** 0.5
+
 
 class TrapezoidalNumber:
     def __init__(self, low, mid_low, mid_high, high):
@@ -52,8 +55,14 @@ class FuzzyPairwiseComparisonMatrix:
     def AlphaLevel(self, alpha):
         return FuzzyPairwiseComparisonMatrix(self.n,
                                              [[self.matrix[i][j].AlphaLevel(alpha) if i != j else 1
-                                               for i in range(0, self.n)]
-                                              for j in range(0, self.n)])
+                                               for j in range(0, self.n)]
+                                              for i in range(0, self.n)])
+
+    # def GenerateFromRow(self,row_index):
+    #     return FuzzyPairwiseComparisonMatrix(self.n,
+    #                                          [[self.matrix[i][j].AlphaLevel(alpha) if i != j else 1
+    #                                            for j in range(0, self.n)]
+    #                                           for i in range(0, self.n)])
 
     def Consistency(self):
         consistent = True
@@ -69,6 +78,7 @@ class FuzzyPairwiseComparisonMatrix:
                     consistent = consistent and max_low_produce <= min_high_produce
         return consistent
 
+_1 = TrapezoidalNumber(1,1,1,1)
 one = TrapezoidalNumber(1. / 3, 0.5, 1.5, 2)
 two = TrapezoidalNumber(1, 1.5, 2.5, 3)
 three = TrapezoidalNumber(2, 2.5, 3.5, 4)
@@ -79,24 +89,24 @@ seven = TrapezoidalNumber(6, 6.5, 7.5, 8)
 eight = TrapezoidalNumber(7, 7.5, 8.5, 9)
 nein = TrapezoidalNumber(8, 9, 9, 9)
 
-criteria_fpcm = FuzzyPairwiseComparisonMatrix(3, [[1, two.Inverse(), three.Inverse()],
-                                                  [two, 1, three.Inverse()],
-                                                  [three, three, 1]])
+criteria_fpcm = FuzzyPairwiseComparisonMatrix(3, [[_1, two.Inverse(), three.Inverse()],
+                                                  [two, _1, three.Inverse()],
+                                                  [three, three, _1]])
 
-alternative_fpcm_by_crit_1 = FuzzyPairwiseComparisonMatrix(4, [[1, one, one, three],
-                                                               [one, 1, three, one],
-                                                               [one, three.Inverse(), 1, two],
-                                                               [three.Inverse(), one, two.Inverse(), 1]])
+alternative_fpcm_by_crit_1 = FuzzyPairwiseComparisonMatrix(4, [[_1, one, one, three],
+                                                               [one.Inverse(), _1, three, one],
+                                                               [one.Inverse(), three.Inverse(), _1, two],
+                                                               [three.Inverse(), one.Inverse(), two.Inverse(), _1]])
 
-alternative_fpcm_by_crit_2 = FuzzyPairwiseComparisonMatrix(4, [[1, three, three, three],
-                                                               [three.Inverse(), 1, three.Inverse(), three.Inverse()],
-                                                               [three.Inverse(), three, 1, one],
-                                                               [three.Inverse(), three, one, 1]])
+alternative_fpcm_by_crit_2 = FuzzyPairwiseComparisonMatrix(4, [[_1, three, three, three],
+                                                               [three.Inverse(), _1, three.Inverse(), three.Inverse()],
+                                                               [three.Inverse(), three, _1, one],
+                                                               [three.Inverse(), three, one.Inverse(), _1]])
 
-alternative_fpcm_by_crit_3 = FuzzyPairwiseComparisonMatrix(4, [[1, one, three.Inverse(), two],
-                                                               [one, 1, three.Inverse(), three],
-                                                               [three, three, 1, five],
-                                                               [two.Inverse(), three.Inverse(), five.Inverse(), 1]])
+alternative_fpcm_by_crit_3 = FuzzyPairwiseComparisonMatrix(4, [[_1, one, three.Inverse(), two],
+                                                               [one.Inverse(), _1, three.Inverse(), three],
+                                                               [three, three, _1, five],
+                                                               [two.Inverse(), three.Inverse(), five.Inverse(), _1]])
 
 for key in globals().keys():
     if "fpcm" in key:
@@ -109,3 +119,4 @@ for key in globals().keys():
         print globals()[key].AlphaLevel(0.5)
         print "consistent " + str(globals()[key].AlphaLevel(0.5).Consistency())
         print
+
